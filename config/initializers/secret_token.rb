fa07db925f158fb28f055913d79f2b4119f83a9c
Anil Sharma <sharma.ruby1git@sharmail.com>
2015-12-16 11:30:10 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ship::Application.config.secret_key_base = 'bbb5cc15a3e814f717b069232bc2d942061433423c35ff1c37d8198c0081d815ea5e983080adfc6e46384798b87a541708146007b551838470e92acf48f6e7a5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ship::Application.config.session_store :cookie_store, key: '_ship_session'
