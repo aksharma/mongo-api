4ffe909843fd2cbd0ae9d5931c53e017082c8ebb
Anil Sharma <sharma.ruby1git@sharmail.com>
2015-12-16 13:52:19 -0500

    Minor cleanup of tests.

--- spec/requests/products_spec.rb
+++ spec/requests/products_spec.rb
@@ -2,28 +2,30 @@ describe 'Products API' do
   before :each do
     Product.delete_all
   end
-  describe 'POST /products' do
-    let :post_params do
-                        {product: {name: "Small Package",
-                                   type: "Golf",
-                                   length: 48,
-                                   width: 14,
-                                   height: 12,
-                                   weight: 42}}
 
+  let :post_params do
+                      {product: {name: "Small Package",
+                                 type: "Golf",
+                                 length: 48,
+                                 width: 14,
+                                 height: 12,
+                                 weight: 42}}
+
+  end
     end
+  describe 'POST /products' do
     it 'Creates a product' do
       post '/products', post_params
 
       expect(last_response.status).to eq 201
-      body = JSON.parse(last_response.body)
-      expect(body['product']['id']).not_to be_nil
-      expect(body['product']['name']).to eq("Small Package")
-      expect(body['product']['type']).to eq("Golf")
-      expect(body['product']['length']).to eq(48)
-      expect(body['product']['width']).to eq(14)
-      expect(body['product']['height']).to eq(12)
-      expect(body['product']['weight']).to eq(42)
+      json = JSON.parse(last_response.body)
+      expect(json['product']['id']).not_to be_nil
+      expect(json['product']['name']).to eq("Small Package")
+      expect(json['product']['type']).to eq("Golf")
+      expect(json['product']['length']).to eq(48)
+      expect(json['product']['width']).to eq(14)
+      expect(json['product']['height']).to eq(12)
+      expect(json['product']['weight']).to eq(42)
     end
 
     it 'Returns an error message when a name is not provided' do
@@ -31,8 +33,8 @@ describe 'Products API' do
       post '/products', post_params
 
       expect(last_response.status).to eq 422
-      body = JSON.parse(last_response.body)
-      expect(body['errors']).to eq({'name' => ["can't be blank"]})
+      json = JSON.parse(last_response.body)
+      expect(json['errors']).to eq({'name' => ["can't be blank"]})
     end
 
     it 'Returns an error message when a type is not provided' do
@@ -40,8 +42,8 @@ describe 'Products API' do
       post '/products', post_params
 
       expect(last_response.status).to eq 422
-      body = JSON.parse(last_response.body)
-      expect(body['errors']).to eq({'type' => ["can't be blank"]})
+      json = JSON.parse(last_response.body)
+      expect(json['errors']).to eq({'type' => ["can't be blank"]})
     end
 
     it 'Returns an error message when a name is already taken' do
@@ -49,8 +51,8 @@ describe 'Products API' do
       post '/products', post_params
 
       expect(last_response.status).to eq 422
-      body = JSON.parse(last_response.body)
-      expect(body['errors']).to eq({'name' => ["has already been taken"]})
+      json = JSON.parse(last_response.body)
+      expect(json['errors']).to eq({'name' => ["has already been taken"]})
     end
 
     it 'Returns an error message when a length is not a number' do
@@ -58,8 +60,8 @@ describe 'Products API' do
       post '/products', post_params
 
       expect(last_response.status).to eq 422
-      body = JSON.parse(last_response.body)
-      expect(body['errors']).to eq({'length' => ["is not a number"]})
+      json = JSON.parse(last_response.body)
+      expect(json['errors']).to eq({'length' => ["is not a number"]})
     end
 
     it 'Returns an error message when a length is not an integer' do
@@ -67,8 +69,8 @@ describe 'Products API' do
       post '/products', post_params
 
       expect(last_response.status).to eq 422
-      body = JSON.parse(last_response.body)
-      expect(body['errors']).to eq({'length' => ["must be an integer"]})
+      json = JSON.parse(last_response.body)
+      expect(json['errors']).to eq({'length' => ["must be an integer"]})
     end
 
     it 'Returns an error message when a width is not a number' do
@@ -76,8 +78,8 @@ describe 'Products API' do
       post '/products', post_params
 
       expect(last_response.status).to eq 422
-      body = JSON.parse(last_response.body)
-      expect(body['errors']).to eq({'width' => ["is not a number"]})
+      json = JSON.parse(last_response.body)
+      expect(json['errors']).to eq({'width' => ["is not a number"]})
     end
 
     it 'Returns an error message when a width is not an integer' do
@@ -85,8 +87,8 @@ describe 'Products API' do
       post '/products', post_params
 
       expect(last_response.status).to eq 422
-      body = JSON.parse(last_response.body)
-      expect(body['errors']).to eq({'width' => ["must be an integer"]})
+      json = JSON.parse(last_response.body)
+      expect(json['errors']).to eq({'width' => ["must be an integer"]})
     end
 
     it 'Returns an error message when a height is not a number' do
@@ -94,8 +96,8 @@ describe 'Products API' do
       post '/products', post_params
 
       expect(last_response.status).to eq 422
-      body = JSON.parse(last_response.body)
-      expect(body['errors']).to eq({'height' => ["is not a number"]})
+      json = JSON.parse(last_response.body)
+      expect(json['errors']).to eq({'height' => ["is not a number"]})
     end
 
     it 'Returns an error message when a height is not an integer' do
@@ -103,8 +105,8 @@ describe 'Products API' do
       post '/products', post_params
 
       expect(last_response.status).to eq 422
-      body = JSON.parse(last_response.body)
-      expect(body['errors']).to eq({'height' => ["must be an integer"]})
+      json = JSON.parse(last_response.body)
+      expect(json['errors']).to eq({'height' => ["must be an integer"]})
     end
 
     it 'Returns an error message when a weight is not a number' do
@@ -112,8 +114,8 @@ describe 'Products API' do
       post '/products', post_params
 
       expect(last_response.status).to eq 422
-      body = JSON.parse(last_response.body)
-      expect(body['errors']).to eq({'weight' => ["is not a number"]})
+      json = JSON.parse(last_response.body)
+      expect(json['errors']).to eq({'weight' => ["is not a number"]})
     end
 
     it 'Returns an error message when a weight is not an integer' do
@@ -121,8 +123,8 @@ describe 'Products API' do
       post '/products', post_params
 
       expect(last_response.status).to eq 422
-      body = JSON.parse(last_response.body)
-      expect(body['errors']).to eq({'weight' => ["must be an integer"]})
+      json = JSON.parse(last_response.body)
+      expect(json['errors']).to eq({'weight' => ["must be an integer"]})
     end
 
   end
