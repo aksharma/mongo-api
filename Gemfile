508aeab377e91fdba85ad3e3da91e4dd35384f8d
Anil Sharma <sharma.ruby1git@sharmail.com>
2015-12-16 12:06:24 -0500

    Added mongo_mapper gem and generated config.

--- Gemfile
+++ Gemfile
@@ -1,5 +1,8 @@
 source 'https://rubygems.org'
 
+gem 'mongo_mapper'
+gem 'bson_ext'
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.0'
 
--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,9 @@ Bundler.require(:default, Rails.env)
 
 module Ship
   class Application < Rails::Application
+    config.generators do |g|
+        g.orm :mongo_mapper
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
